12f392381e19e1bad23beaf7ac23b5e72aa93580
Son Tran <tbson87@gmail.com>
2019-03-14 06:13:00 +0700

    Code spa.

--- client/src/utils/components/__tests__/LoadingLabel.test.js
+++ client/src/utils/components/__tests__/LoadingLabel.test.js
@@ -1,19 +1,20 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import React from 'react';
-import { shallow, mount, render } from 'enzyme';
+import {shallow} from 'enzyme';
 import LoadingLabel from '../LoadingLabel';
 
-Enzyme.configure({ adapter: new Adapter() });
-
+Enzyme.configure({adapter: new Adapter()});
 
 describe('LoadingLabel component', () => {
     it('should render without throwing an error', () => {
-        const wrapper = shallow(<LoadingLabel/>);
-        expect(wrapper.contains(
-                <div className="alert alert-info" role="alert">Loading data...</div>
+        const wrapper = shallow(<LoadingLabel />);
+        expect(
+            wrapper.contains(
+                <div className="alert alert-info" role="alert">
+                    Loading data...
+                </div>
             )
-        ).toEqual(true)
+        ).toBe(true);
     });
 });
-
