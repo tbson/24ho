e5ce72d79c161787ccabafa74f2d8cb88e1b1d83
Son Tran <tbson87@gmail.com>
2019-04-05 15:04:40 +0700

    Fix failing test cases.

--- client/src/utils/components/form/ErrorMessages.js
+++ client/src/utils/components/form/ErrorMessages.js
@@ -7,7 +7,7 @@ type Props = {
 };
 export default ({errors, alert = false}: Props) => {
     const errorItems = errors => errors.map((error, key) => <li key={key}>{error}</li>);
-    const errorItem = error => <>{error}</>;
+    const errorItem = error => <div>{error}</div>;
     return Array.isArray(errors) && errors.length ? (
         <div
             className={alert ? 'alert alert-danger' : 'invalid-feedback'}
