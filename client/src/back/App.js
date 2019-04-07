99c30be46c44720cd37be0cf5c70bb7a8c444de0
Son Tran <tbson87@gmail.com>
2019-04-07 15:48:34 +0700

    Use PrivateRoute to check authentication before component rendering.

--- client/src/back/App.js
+++ client/src/back/App.js
@@ -14,7 +14,6 @@ import {ToastContainer} from 'react-toastify';
 import(/* webpackPreload: true */ 'rummernote/build/bs4/style.css');
 // $FlowFixMe: do not complain about importing node_modules
 import(/* webpackPreload: true */ 'bootstrap/dist/js/bootstrap');
-
 import 'src/utils/styles/main-back.css';
 import Spinner from 'src/utils/components/Spinner';
 import NotMatch from 'src/utils/components/route/NotMatch';
@@ -59,14 +58,14 @@ class App extends React.Component<Props> {
                 <Spinner />
                 <ToastContainer autoClose={5000} />
                 <Switch>
-                    <Route exact path="/" component={Profile} />
                     <Route path="/login" component={Login} />
                     <Route path="/reset-password/:token" component={ResetPwd} />
-                    <Route path="/variable" component={Variable} />
-                    <Route path="/staff" component={Staff} />
-                    <Route path="/customer" component={Customer} />
-                    <Route path="/area" component={Area} />
-                    <Route path="/address" component={Address} />
+                    <PrivateRoute exact path="/" component={Profile} />
+                    <PrivateRoute path="/variable" component={Variable} />
+                    <PrivateRoute path="/staff" component={Staff} />
+                    <PrivateRoute path="/customer" component={Customer} />
+                    <PrivateRoute path="/area" component={Area} />
+                    <PrivateRoute path="/address" component={Address} />
                     <Route component={NotMatch} />
                 </Switch>
             </div>
--- client/src/utils/components/route/PrivateRoute.js
+++ client/src/utils/components/route/PrivateRoute.js
@@ -6,19 +6,16 @@ import Tools from 'src/utils/helpers/Tools';
 
 type Props = Object;
 
-export default class PrivateRoute extends React.Component<Props> {
-    render() {
-        if (Tools.getToken()) {
-            return <Route {...this.props} />;
-        }
-        return (
-            <Redirect
-                to={{
-                    push: true,
-                    pathname: '/login',
-                    state: {from: this.props.location},
-                }}
-            />
-        );
-    }
-}
+export default (props: Props) => {
+    if (Tools.getToken()) return <Route {...props} />;
+
+    return (
+        <Redirect
+            to={{
+                push: true,
+                pathname: '/login',
+                state: {from: props.location}
+            }}
+        />
+    );
+};
