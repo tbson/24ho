d91320b9893d63adbabb32122e0290ba8baa81a7
Son Tran <tbson87@gmail.com>
2019-04-30 21:39:21 +0700

    NavWrapper use functional style.

--- client/src/utils/components/nav_wrapper/index.js
+++ client/src/utils/components/nav_wrapper/index.js
@@ -1,7 +1,8 @@
 // @flow
 import * as React from 'react';
+import {useState, useEffect} from 'react';
 // $FlowFixMe: do not complain about importing node_modules
-import {withRouter, Route, Link} from 'react-router-dom';
+import {withRouter} from 'react-router-dom';
 // $FlowFixMe: do not complain about importing node_modules
 import {NavLink} from 'react-router-dom';
 import Tools from 'src/utils/helpers/Tools';
@@ -14,123 +15,96 @@ type Props = {
     children: React.Node
 };
 
-type State = {
-    toggled: boolean,
-    mql: Object
-};
+const App = ({history, location, children}: Props) => {
+    const [toggled, setToggled] = useState(window.innerWidth >= 768 ? true : false);
 
-class App extends React.Component<Props, State> {
-    logout: Function;
-    state = {
-        toggled: true,
-        mql: {}
+    const logout = Tools.logout(history);
+
+    useEffect(() => {
+        const mlq = window.matchMedia(`(min-width: 800px)`);
+        mlq.addListener(mediaQueryChanged);
+        return () => {
+            mlq.removeListener(mediaQueryChanged);
+        };
+    }, []);
+
+    const mediaQueryChanged = () => {
+        setToggled(!toggled);
     };
 
-    constructor(props) {
-        super(props);
-        this.logout = Tools.logout(this.props.history);
-    }
-
-    componentDidMount() {
-        const mql = window.matchMedia(`(min-width: 800px)`);
-        mql.addListener(this.mediaQueryChanged);
-        this.setState({mql: mql});
-
-        this.setState({
-            toggled: window.innerWidth >= 800 ? true : false
-        });
-    }
-
-    componentWillUnmount() {
-        this.state.mql.removeListener(this.mediaQueryChanged);
-    }
-
-    mediaQueryChanged = () => {
-        this.setState({
-            toggled: !this.state.toggled
-        });
+    const toggleAll = () => {
+        setToggled(!toggled);
     };
 
-    toggleAll = () => {
-        this.setState({
-            toggled: !this.state.toggled
-        });
-    };
+    const menuItem = (url: string, title: string, icon: string) => (
+        <li>
+            <NavLink exact to={`/${url}`}>
+                <i className={icon} />
+                {toggled ? <span>&nbsp;&nbsp;{title}</span> : null}
+            </NavLink>
+        </li>
+    );
 
-    menuItem(url: string, title: string, icon: string) {
-        return (
-            <li>
-                <NavLink exact to={`/${url}`}>
-                    <i className={icon} />
-                    &nbsp;&nbsp;
-                    <span>{title}</span>
-                </NavLink>
-            </li>
-        );
-    }
-
-    renderMenu = (menu: string) => {
+    const renderMenu = (menu: string) => {
         switch (menu) {
             case 'profile':
-                return this.menuItem('', 'Profile', 'fas fa-user');
+                return menuItem('', 'Profile', 'fas fa-user');
             case 'admin':
                 if (APP !== 'admin') return null;
-                return this.menuItem('staff', 'Admin', 'fas fa-user-secret');
+                return menuItem('staff', 'Admin', 'fas fa-user-secret');
             case 'customer':
                 if (APP !== 'admin') return null;
-                return this.menuItem('customer', 'Customer', 'fas fa-user-ninja');
+                return menuItem('customer', 'Customer', 'fas fa-user-ninja');
             case 'variable':
                 if (APP !== 'admin') return null;
-                return this.menuItem('variable', 'Variable', 'fas fa-cog');
+                return menuItem('variable', 'Variable', 'fas fa-cog');
             case 'area':
                 if (APP !== 'admin') return null;
-                return this.menuItem('area', 'Area', 'fas fa-map-marker-alt');
+                return menuItem('area', 'Area', 'fas fa-map-marker-alt');
             case 'address':
                 if (APP !== 'user') return null;
-                return this.menuItem('address', 'Address', 'fas fa-map-marker-alt');
+                return menuItem('address', 'Address', 'fas fa-map-marker-alt');
             case 'cart':
                 if (APP !== 'user') return null;
-                return this.menuItem('cart', 'Cart', 'fas fa-shopping-cart');
+                return menuItem('cart', 'Cart', 'fas fa-shopping-cart');
             case 'rate':
                 if (APP !== 'admin') return null;
-                return this.menuItem('rate', 'Rate', 'fas fa-yen-sign');
+                return menuItem('rate', 'Rate', 'fas fa-yen-sign');
         }
     };
 
-    render() {
-        const {renderMenu} = this;
-        const user = Tools.getStorageObj('auth');
-        const fullname = `${user.last_name} ${user.first_name}`;
-        return (
-            <div id="wrapper" className={this.state.toggled ? 'toggled' : ''}>
-                <div id="sidebar-wrapper">
-                    <div className="sidebar-brand">24HOrder</div>
-                    <ul className="sidebar-nav">
-                        {renderMenu('profile')}
-                        {renderMenu('admin')}
-                        {renderMenu('customer')}
-                        {renderMenu('variable')}
-                        {renderMenu('area')}
-                        {renderMenu('address')}
-                        {renderMenu('cart')}
-                        {renderMenu('rate')}
-                    </ul>
-                </div>
+    const user = Tools.getStorageObj('auth');
+    const fullname = `${user.last_name} ${user.first_name}`;
 
-                <div id="page-content-wrapper">
-                    <div id="main-heading">
-                        <span id="nav-toggler" onClick={this.toggleAll}>
-                            &#9776;
-                        </span>
-                        <span>{fullname}</span>
-                        &nbsp;&nbsp;
-                        <i className="fas fa-sign-out-alt pointer" onClick={() => this.logout()} />
-                    </div>
-
-                    <div className="container-fluid">{this.props.children}</div>
-                </div>
+    return (
+        <div id="wrapper" className={toggled ? 'toggled' : ''}>
+            <div id="sidebar-wrapper">
+                <div className="sidebar-brand">24HOrder</div>
+                <ul className="sidebar-nav">
+                    {renderMenu('profile')}
+                    {renderMenu('admin')}
+                    {renderMenu('customer')}
+                    {renderMenu('variable')}
+                    {renderMenu('area')}
+                    {renderMenu('address')}
+                    {renderMenu('cart')}
+                    {renderMenu('rate')}
+                </ul>
             </div>
-        );
-    }
-}
+
+            <div id="page-content-wrapper">
+                <div id="main-heading">
+                    <span className="nav-toggler" onClick={toggleAll}>
+                        &#9776;
+                    </span>
+                    <span>{fullname}</span>
+                    &nbsp;&nbsp;
+                    <i className="fas fa-sign-out-alt pointer" onClick={() => logout()} />
+                </div>
+
+                <div className="container-fluid">{children}</div>
+            </div>
+        </div>
+    );
+};
 export default withRouter(App);
