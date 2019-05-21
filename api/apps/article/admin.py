6b2a6b8420a8be790fe52cf37de8685caa5b530b
nguyentiennghiep <nguyen_tien_nghiep@mediado.jp>
2019-05-21 22:34:02 +0700

    fix article model

--- api/apps/article/admin.py
+++ api/apps/article/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Article
 
+
+class ArticleAdmin(admin.ModelAdmin):
+    readonly_fields = ['uid']
+
+
 # Register your models here.
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
--- api/apps/article/models.py
+++ api/apps/article/models.py
@@ -1,18 +1,28 @@
 from django.db import models
-from model_utils.models import TimeStampedModel
+from utils.models.model import TimeStampedModel
 from apps.category.models import Category
 # Create your models here.
 
 
 class Article(TimeStampedModel):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=100, unique=True)
     content = models.TextField()
     uid = models.CharField(max_length=50, unique=True)
     category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name='category')
+        Category, on_delete=models.CASCADE, related_name='title', limit_choices_to={'type': "article"},)
+
+    def save(self, *args, **kwargs):
+
+        # if self._state.adding:
+            # self.category['single'] == True
+
+        #self.uid = self.title.replace(' ', '-')
+        self.uid =  self.category
+
+        super(Article, self).save(*args, **kwargs)
 
     def __str__(self):
-        return '{} - {}'.format(self.article, self.title)
+        return '{}'.format(self.title)
 
     class Meta:
         db_table = "articles"
