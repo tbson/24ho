dbc740f7b7ec78d8012f866849c1cc73dd588938
nguyentiennghiep <nghiepnguyentien@gmail.com>
2019-05-22 19:03:11 +0900

    add article requiments

--- api/apps/article/models.py
+++ api/apps/article/models.py
@@ -1,25 +1,29 @@
 from django.db import models
 from utils.models.model import TimeStampedModel
 from apps.category.models import Category
+from django.core.exceptions import ValidationError
 # Create your models here.
 
-
 class Article(TimeStampedModel):
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name='category', limit_choices_to={'type': "article"}, null=False,)
     title = models.CharField(max_length=100, unique=True)
     content = models.TextField()
     uid = models.CharField(max_length=50, unique=True)
-    category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name='title', limit_choices_to={'type': "article"},)
 
     def save(self, *args, **kwargs):
-
-        # if self._state.adding:
-            # self.category['single'] == True
-
-        #self.uid = self.title.replace(' ', '-')
-        self.uid =  self.category
-
+        self.uid = self.title.replace(' ','-')
+        
         super(Article, self).save(*args, **kwargs)
+
+    def clean(self): 
+        if self.category_id is not None:
+            if(self.category.single == True):
+                if (Article.objects.filter(category__id = self.category_id).exists()):
+                    if(self.uid is None) :
+                        raise ValidationError("Can only create 1 instance of articles when category'single is true.")
+                    else:
+                        
 
     def __str__(self):
         return '{}'.format(self.title)
