74d738afdfada8a40b8b9b05cd984f7ab3ffa87e
nguyentiennghiep <nguyen_tien_nghiep@mediado.jp>
2019-06-03 21:15:53 +0700

    fix model

--- api/apps/article/models.py
+++ api/apps/article/models.py
@@ -7,45 +7,21 @@ from django.core.exceptions import ValidationError
 
 class Article(TimeStampedModel):
     category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name='category', limit_choices_to={'type': "article"}, null=False,)
-    title = models.CharField(max_length=100, unique=True)
+        Category, on_delete=models.CASCADE, related_name='category', limit_choices_to={'type': "article"})
+    title = models.CharField(max_length=250, unique=True)
     content = models.TextField()
-    uid = models.CharField(max_length=50, unique=True)
-    slug = models.CharField(max_length=50, blank=True)
+    uid = models.CharField(max_length=250, unique=True)
+    slug = models.CharField(max_length=250, blank=True)
 
     def save(self, *args, **kwargs):
         self.uid = self.title.replace(' ', '-')
         if self.slug is not None:
             self.slug = self.slug.replace(' ', '-')
 
-        if self.category_id is not None:
-            if(self.category.single == True):
-                if (Article.objects.filter(category__id=self.category_id).exists()):
-                    if(self.uid is None):
-                        raise ValidationError(
-                            "Can only create 1 instance of articles when category'single is true.")
-                    else:
-                        if(Article.objects.filter(category__id=self.category_id)[0].uid != self.uid):
-                            raise ValidationError(
-                                "Can only create 1 instance of articles when category'single is true.")
-
-
         super(Article, self).save(*args, **kwargs)
         if not self.slug:
             self.slug = self.title.replace(' ', '-') + '-' + str(self.pk)
-            super(Article, self).save(*args, **kwargs)
-
-    # def clean(self):
-    #     if self.category_id is not None:
-    #         if(self.category.single == True):
-    #             if (Article.objects.filter(category__id=self.category_id).exists()):
-    #                 if(self.uid is None):
-    #                     raise ValidationError(
-    #                         "Can only create 1 instance of articles when category'single is true.")
-    #                 else:
-    #                     if(Article.objects.filter(category__id=self.category_id)[0].uid != self.uid):
-    #                         raise ValidationError(
-    #                             "Can only create 1 instance of articles when category'single is true.")
+            super().save(*args, **kwargs)
 
     def __str__(self):
         return '{}'.format(self.title)
