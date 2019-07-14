0597e63b13cc133b80deb3e80443f973c6efecc5
Son Tran <tbson87@gmail.com>
2019-07-14 19:21:29 +0700

    Add user sedding script.

---
+++ api/utils/management/commands/user_seeding.py
@@ -0,0 +1,29 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import User
+from apps.staff.models import Staff
+from apps.customer.models import Customer
+
+
+class Command(BaseCommand):
+    help = 'Create root, tbson (staff), sonnl (customer)'
+
+    def handle(self, *args, **options):
+        self.stdout.write(self.style.SUCCESS('Start...'))
+
+        admin = User.objects.create_user(
+            username='admin',
+            is_superuser=True,
+            is_staff=True,
+            email='admin@localhost',
+            password='password'
+        )
+        Staff.objects.create(user=admin)
+
+        user = User.objects.create_user(
+            username='user',
+            email='user@localhost',
+            password='password'
+        )
+        Customer.objects.create(user=user)
+
+        self.stdout.write(self.style.SUCCESS('Done!!!'))
