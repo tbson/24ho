1ef312a9c7f4ba21c08df6381849f699e23c1c84
Son Tran <tbson87@gmail.com>
2019-08-14 20:41:48 +0700

    Superadmin can do everything.

--- api/utils/common_classes/custom_permission.py
+++ api/utils/common_classes/custom_permission.py
@@ -4,6 +4,9 @@ from rest_framework import permissions
 class CustomPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
+        if request.user.is_superuser is True:
+            return True
+
         action = view.action
         name = view._name
         action = action if action not in ['list', 'retrieve', 'retrieve_uid'] else 'view'
