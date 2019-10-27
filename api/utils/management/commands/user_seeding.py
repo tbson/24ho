5575464ae28f07df552fdf440403c3798ce46f57
Son Tran <tbson87@gmail.com>
2019-10-27 19:58:18 +0700

    Update user seeding script.

--- api/utils/management/commands/user_seeding.py
+++ api/utils/management/commands/user_seeding.py
@@ -11,29 +11,60 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Start...'))
+        staffs = [
+            {
+                'username': 'admin',
+                'email': 'admin@localhost.com',
+                'first_name': 'john',
+                'last_name': 'doe',
+                'password': 'password',
+                'is_superuser': True,
+                'is_staff': True
+            },
+            {
+                'username': 'admin1',
+                'email': 'admin1@localhost.com',
+                'first_name': 'jim',
+                'last_name': 'colin',
+                'password': 'password',
+                'is_superuser': True,
+                'is_staff': True
+            }
+        ]
+        for staff_data in staffs:
+            try:
+                User.objects.get(email=staff_data['email'])
+            except User.DoesNotExist:
+                print("[+] Creating admin: {}".format(staff_data['username']))
+                user = User.objects.create_user(**staff_data)
+                # Grand all permission to this user
+                permissions = Permission.objects.all()
+                user.user_permissions.set(permissions)
+                Staff.objects.create(user=user)
 
-        admin = User.objects.create_user(
-            username='admin',
-            is_superuser=True,
-            is_staff=True,
-            email='admin@localhost.com',
-            password='password',
-            first_name='admin',
-            last_name='1',
-        )
-        # Grand all permission to this user
-        permissions = Permission.objects.all()
-        admin.user_permissions.set(permissions)
-        Staff.objects.create(user=admin)
-
-        user = User.objects.create_user(
-            username='user',
-            email='user@localhost.com',
-            password='password',
-            first_name='user',
-            last_name='1',
-        )
-        Customer.objects.create(user=user)
+        customers = [
+            {
+                'username': 'user',
+                'email': 'user@localhost.com',
+                'first_name': 'john',
+                'last_name': 'doe',
+                'password': 'password'
+            },
+            {
+                'username': 'user1',
+                'email': 'user1@localhost.com',
+                'first_name': 'jim',
+                'last_name': 'colin',
+                'password': 'password'
+            }
+        ]
+        for customer_data in customers:
+            try:
+                User.objects.get(email=customer_data['email'])
+            except User.DoesNotExist:
+                print("[+] Creating customer: {}".format(customer_data['username']))
+                user = User.objects.create_user(**customer_data)
+                Customer.objects.create(user=user)
 
         VariableUtils.variable_seeding()
 
